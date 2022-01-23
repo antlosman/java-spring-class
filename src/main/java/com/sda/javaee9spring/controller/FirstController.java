39d9a6b56b3f07d0b90e593c95703737d6031f4f
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-01-23 11:27:14 +0200

    [change] - sending query parameters to endpoint

--- src/main/java/com/sda/javaee9spring/controller/FirstController.java
+++ src/main/java/com/sda/javaee9spring/controller/FirstController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
@@ -13,7 +14,7 @@ public class FirstController {
     // the same as @Slf4j
 //    private static final Logger log = LoggerFactory.getLogger(FirstController.class);
 
-    @GetMapping("/my-first-page")
+    @GetMapping({"/my-first-page", "/"})
     public String firstPage() {
         log.info("firstPage() method called!!!");
         // looking for html page inside resources/templates
@@ -28,11 +29,25 @@ public class FirstController {
         return "second-page";
     }
 
-    @GetMapping("/my-third-page")
+//    @GetMapping("/my-third-page")
+//    @GetMapping(value = "/my-third-page")
+//    @GetMapping(value = {"/my-third-page"})
+    @GetMapping(value = {"/my-third-page","my-third-page-too"})
     public String thirdPage() {
         log.info("thirdPage() method called!!!");
         // looking for html page inside resources/templates
         return "pages/third-page";
+    }
+
+    // /my-name?name=anton&surname=losman
+    // name param with value anton
+    // surname param with value losman
+    @GetMapping("/my-name")
+    public String myName(@RequestParam("name") String myName, @RequestParam("surname") String mySurname) {
+        log.info("myName() method was called!");
+        log.info("my name is: [{}] and my surname is: [{}]", myName, mySurname);
+        log.info(String.format("my name is: [%s] and my surname is: [%s]", myName, mySurname));
+        return "";
     }
 
 }
