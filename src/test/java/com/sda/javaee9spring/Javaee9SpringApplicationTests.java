3570c9a444570e25a958ebc41ce37ac0ec2b6396
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-02-01 17:03:57 +0200

    [change] - examples of creating spring beans

--- src/test/java/com/sda/javaee9spring/Javaee9SpringApplicationTests.java
+++ src/test/java/com/sda/javaee9spring/Javaee9SpringApplicationTests.java
@@ -1,5 +1,7 @@
 package com.sda.javaee9spring;
 
+import net.minidev.json.JSONUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,6 +22,35 @@ class Javaee9SpringApplicationTests {
         // dependency is actually composition, so one item is made of another item
         Parent jason = new Parent("Jason", john);
     }
+
+    @Test
+    void recordTest() {
+        // use records instead of value from Lombok
+        // records are immutable - cannot modify instances of the class - so setters
+        Toy plasticGun = new Toy("pGun");
+        System.out.println(plasticGun);
+
+        // child.with(favoriteToy = plasticGun); // in future Java version
+    }
+
+    @Test
+    void equalTest() {
+        Assertions.assertTrue(4 == 4);
+
+        Animal one = new Animal();
+        Animal two = new Animal();
+        Animal three = one;
+
+        // == checks if this is the same object, checks if two references point to the same object
+        // equals by default checks if this is the same object, cheks if two object have same content
+        Assertions.assertFalse(one == two);
+        Assertions.assertFalse(one.equals(two));
+
+        Toy pGun = new Toy("pGun");
+        Toy pGun2 = new Toy("pGun");
+        Assertions.assertEquals(pGun,pGun2);
+    }
+
 }
 
 // toy is made of name
@@ -28,4 +59,8 @@ record Toy(String name) {}
 record Child(String name, Toy favoriteToy) {}
 // parent is made of (composed of) name and child
 record Parent(String name, Child child) {
+}
+
+class Animal {
+
 }
---
+++ src/test/java/com/sda/javaee9spring/SpringContextTest.java
@@ -0,0 +1,40 @@
+package com.sda.javaee9spring;
+
+import com.sda.javaee9spring.component.MyFirstSpringComponent;
+import com.sda.javaee9spring.controller.FirstController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class SpringContextTest {
+    @Autowired
+    private FirstController myFirstController;
+
+    @Test
+    void checkFirstControllerReference() {
+        assertNotNull(myFirstController);
+        String firstPageName = myFirstController.firstPage();
+        assertEquals("home-page", firstPageName);
+    }
+
+    @Autowired
+    private MyFirstSpringComponent myFirstSpringComponent;
+
+    @Autowired
+    private MyFirstSpringComponent myFirstSpringComponentSecond;
+
+    @Test
+    void checkMyFirstSpringComponentReference() {
+        assertNotNull(myFirstSpringComponent);
+    }
+
+    @Test
+    void manyObjectsOfTheSameTypeTest() {
+        assertSame(myFirstSpringComponent, myFirstSpringComponentSecond);
+        // == - checks if two references points to the same object
+        // by default every bean inside Spring Context is singleton
+    }
+}
