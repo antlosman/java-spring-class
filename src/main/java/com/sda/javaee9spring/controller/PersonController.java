41ae43913bfcc2dd0dd9c6bfa437c4a162f6e958
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-02-02 19:09:31 +0200

    [change] - refactoring of PersonController + some "git" tip added

--- src/main/java/com/sda/javaee9spring/controller/PersonController.java
+++ src/main/java/com/sda/javaee9spring/controller/PersonController.java
@@ -1,13 +1,12 @@
 package com.sda.javaee9spring.controller;
 
-import com.sda.javaee9spring.entity.Person;
+import com.sda.javaee9spring.service.PersonService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @Slf4j
@@ -15,27 +14,18 @@ import java.util.List;
 public class PersonController {
 
     public static final String PERSONS_KEY = "persons";
-    // with var Java is going to guess type of the variable based on assigned value
-//        var name = "mariusz"; // String name = "mariusz";
-//        var mariusz = new Person("Mariusz", "Pastuszka", 18);
-//        Person mariusz = new Person("Mariusz", "Pastuszka", 18);
-//        var mariusz = null; // need initialization value, without that it won't work
-//    ArrayList<Person> myBestFriends = new ArrayList<Person>(); // till Java 7
-//    ArrayList<Person> myBestFriendsJava7 = new ArrayList<>(); // since Java 7
-//    var myBestFriendsJava10 = new ArrayList<Person>(); // since Java 10
-//    var myBestFriendsJava10v2 = new ArrayList<>(); // since Java 10
-//    var myBestFriendsJava10v3 = new ArrayList<Object>(); // since Java 10
 
-    // this will be dynamic in the future
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
     @GetMapping("/names")
     public String showListOfPersonsNames(Model data) {
-        //        List<Person> persons
-        var persons
-                = List.of(
-                new Person("Mariusz", "Pastuszka", 18),
-                new Person("Alicja", "Strus", 25),
-                new Person("Joe", "Doe", 40)
-        );
+
+        var persons = personService.getAllPersons();
 
         data.addAttribute(PERSONS_KEY, persons);
         return "persons/persons-names";
