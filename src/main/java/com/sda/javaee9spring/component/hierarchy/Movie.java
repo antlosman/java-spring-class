450be74cbcf2f0303069917d8d94f18636553920
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-02-05 17:31:06 +0200

    [fix] - some bugs in Movie class and Movie repository are fixed

--- src/main/java/com/sda/javaee9spring/component/hierarchy/Movie.java
+++ src/main/java/com/sda/javaee9spring/component/hierarchy/Movie.java
@@ -1,13 +1,4 @@
 package com.sda.javaee9spring.component.hierarchy;
 
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public record Movie() {
-
-    public List<Movie>  readAllMoviesFromDatabase() {
-        return List.of();
-    }
+public record Movie(String title) {
 }
--- src/main/java/com/sda/javaee9spring/component/hierarchy/MovieRepository.java
+++ src/main/java/com/sda/javaee9spring/component/hierarchy/MovieRepository.java
@@ -2,6 +2,12 @@ package com.sda.javaee9spring.component.hierarchy;
 
 import org.springframework.stereotype.Repository;
 
-//@Repository
+import java.util.List;
+
+@Repository
 public class MovieRepository {
+
+     public List<Movie> readAllMoviesFromDatabase() {
+         return List.of();
+     }
 }
