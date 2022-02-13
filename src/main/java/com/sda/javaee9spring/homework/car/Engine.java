8b82d539846e56e0ae772a60f98d2a817d45895a
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-02-13 10:55:20 +0200

    [change] - added mvc structure in practice and Component annotations in homework package

--- src/main/java/com/sda/javaee9spring/homework/car/Engine.java
+++ src/main/java/com/sda/javaee9spring/homework/car/Engine.java
@@ -1,4 +1,7 @@
 package com.sda.javaee9spring.homework.car;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public record Engine(EngineHead engineHead) {
 }
--- src/main/java/com/sda/javaee9spring/homework/car/Wheel.java
+++ src/main/java/com/sda/javaee9spring/homework/car/Wheel.java
@@ -2,7 +2,9 @@ package com.sda.javaee9spring.homework.car;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component
 @Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public record Wheel() {
 }
